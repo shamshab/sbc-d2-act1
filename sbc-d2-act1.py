678959bcad5f401c00ba86352fce11dc13df01c1
shamshab <shamon.salvaloza@gmail.com>
2024-06-19 13:40:49 +0800

    act1

---
+++ sbc-d2-act1.py
@@ -0,0 +1,19 @@
+happy=("summer bootcamp")
+
+print(happy.title())
+print(happy.capitalize().replace("p", "P"))
+print("{0}{1}" .format (happy[11:14], happy[4].upper()))
+print(happy[7:11].replace("b", "L"))
+print("{1}{0}" .format (happy[5].upper(), happy[12].upper()))
+print(happy.replace(" ", "").isalpha())
+
+
+#s = happy(["0:5"])+(["5:12"])
+#s = happy.lower([5]) + happy.upper([12])
+#happy = happy.lower([12]) + happy.upper([5])
+#print(happy.upper("s,p"))
+#print(happy.index("a","r"))
+#print(happy.find("L"))
+#print(happy.replace())
+#print(happy.replace())
+
